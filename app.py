d020a8bdd395b1a5bed514e905d47ad180c38da4
Daniel Johnson <dlj7m7@mst.edu>
2018-10-14 06:32:46 -0500

    Finished sign up buttons

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
+from sqlalchemy.orm import sessionmaker
+from tabledef import *
+engine = create_engine('sqlite:///tutorial.db', echo=True)
+
+tempName = "Not working"
 
 app = Flask(__name__)
 
@@ -11,11 +16,44 @@ def home():
     else:
         return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        POST_USERNAME = str(request.form['username'])
+        POST_PASSWORD = str(request.form['password'])
+
+        Session = sessionmaker(bind=engine)
+        s = Session()
+        query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+        result = query.first()
+        if result:
+            session['logged_in'] = True
+            return render_template('index.html')
+        else:
+            flash('wrong password!')
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        if request.form['confirmpassword'] == request.form['password'] and request.form['username'] != '' and request.form['e-mail'] != '' and request.form['password'] != '':
+            session['logged_in'] = True
+            tempName = request.form['username']
+            #print(tempName)
+            #return render_template('index.html')
+            print(tempName)
+            return render_template('index.html', tempName = tempName)
+    return render_template('signup.html')
+
+
 @app.route('/index.html')
 def index():
     if not session.get('logged_in'):
+        print("not logged")
         return render_template('index.html')
     else:
+        print("logged")
+        #request.form['User'] = tempName
         return render_template('index.html')
 
 @app.route('/political.html')
@@ -43,16 +81,16 @@ def health():
 def loggedout():
     return render_template('loggedout.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        if request.form['password'] == 'password' and request.form['username'] == 'admin':
-            username = 'admin'
-            session['logged_in'] = True
-            return home()
-        else:
-            flash('wrong password!')
-    return render_template('login.html')
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         if request.form['password'] == 'password' and request.form['username'] == 'admin':
+#             username = 'admin'
+#             session['logged_in'] = True
+#             return home()
+#         else:
+#             flash('wrong password!')
+#     return render_template('login.html')
 
 @app.route("/logout")
 def logout():
